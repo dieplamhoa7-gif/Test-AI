d4bdf0e3526716ea082a3c083629407be94dfd9e
Hoa D - CVDT <hoa.diep@nkpros.vn>
2026-06-14 00:20:50 +0700

    Set Python and UTF-8 env for Render

--- render_start.js
+++ render_start.js
@@ -23,6 +23,10 @@ function findPlaywrightChrome() {
 }
 
 process.env.NVTC_PROXY_PORT = process.env.PORT || process.env.NVTC_PROXY_PORT || '10000';
+process.env.PYTHON = process.env.PYTHON || '/usr/bin/python3';
+process.env.PYTHONIOENCODING = process.env.PYTHONIOENCODING || 'utf-8';
+process.env.LANG = process.env.LANG || 'C.UTF-8';
+process.env.LC_ALL = process.env.LC_ALL || 'C.UTF-8';
 process.env.BDS_BROWSER_CDP = process.env.BDS_BROWSER_CDP || 'http://127.0.0.1:18800';
 process.env.BDS_BROWSER_PROFILE = process.env.BDS_BROWSER_PROFILE || '/tmp/lh-bds-browser-profile';
 process.env.BDS_CHROME_PATH = process.env.BDS_CHROME_PATH || findPlaywrightChrome();
